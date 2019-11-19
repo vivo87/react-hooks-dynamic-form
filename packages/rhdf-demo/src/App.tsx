a9d8a9780cccf1595689084f4e8db487cabce096
vivo87 <vongocviet@gmail.com>
2019-11-19 12:06:09 +0100

    eslint

---
+++ packages/rhdf-demo/src/App.tsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+
+import { Form } from "react-hooks-dynamic-form";
+import "./App.css";
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <Form />
+    </div>
+  );
+};
+
+export default App;
--- packages/rhdf-lib/src/index.ts
+++ packages/rhdf-lib/src/index.ts
@@ -1,1 +1,3 @@
 export const Greeter = (name: string) => `Hello ${name}`;
+
+export { default as Form } from "./components/Form";

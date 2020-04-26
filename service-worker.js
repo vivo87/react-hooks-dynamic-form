53d47a41a5a2cb149db0dd78abb817ea1d9fa135
vivo87 <vongocviet@gmail.com>
2020-04-26 22:21:55 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.3795c0c7ecea00b4eb3a920cd6a182ef.js"
+  "precache-manifest.b6f8c4a80775eabcb54c8b8cfdcccfea.js"
 );
 
 self.addEventListener('message', (event) => {
